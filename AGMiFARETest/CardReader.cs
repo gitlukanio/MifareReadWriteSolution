4f3b3f0fca9314e923a7c2cad5bb9f223eb05962
Łukasz Łuczko <lukasz.luczko@gmail.com>
2017-11-22 13:26:21 +0100

    Initial full, all clases imported

---
+++ AGMiFARETest/CardReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AG.MiFARE;
+//using ConsoleACR122U_2;
+
+namespace AGMiFARETest
+{
+    public class CardReader : cardConnector, ICardReader
+    {
+        //Byte[] _Data;
+        public bool GetCardType(out CardTypeEnum cardType)
+        {
+            if (CardExist())
+            {
+                cardType = CardTypeEnum.MiFARE_1K;
+                return true;
+            }
+            else
+            {
+                cardType = CardTypeEnum.Unknown;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Login into the given sector using the given key
+        /// </summary>
+        /// <param name="sector">sector to login into</param>
+        /// <param name="key">key to use</param>
+        /// <returns>tru on success, false otherwise</returns>
+        public bool Login(int sector, KeyTypeEnum key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read(int sector, int datablock, out byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Write(int sector, int datablock, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
